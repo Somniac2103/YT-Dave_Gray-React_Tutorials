600c6374b4234064c55cf9cd72fc7a9f8b0c62cc
Somniac2103 <salmon.barry1@gmail.com>
2023-12-28 18:05:37 +0700

    Continued with React Training Course
    You Tube Course called React JS Full Course for Beginners by Professor Dave Gray
    
    Lesson Completed in this session:
    
    Lesson 5 : Click Events
    Lesson 6 : useState Hook
    Lesson 7 : Lists & Keys

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,16 +1,42 @@
-const Content = () => {
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa'
 
-  const handleNameChange = () => {
-    const name = ['Bob', 'Kevin', 'Dave'];
-    const int = Math.floor(Math.random()*3);
-    return name[int];};
-    
+const Content = () => {
+  const [items, setItems] = useState([
+    {
+      id:1,
+      checked: false,
+      item: "One half pound of Cocoa Covered"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3"
+    }    
+  ]);
+      
   return (
     <main>
-      <p>
-        Hello {handleNameChange()}!
-      </p>
-
+      <ul>
+        {items.map((item) => (
+          <li className='item' key={item.id}>
+            <input 
+              type="checkbox" 
+              checked={item.checked}
+            />
+            <label>{item.item}</label>
+            <FaTrashAlt 
+              role="button" 
+              tabIndex="0"
+            />
+           </li>
+        ))}
+      </ul>
     </main>
   )
 }
