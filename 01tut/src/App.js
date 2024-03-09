cdd75d9aec14311051190895810e6a64b7e522d0
Barry Keith Salmon <salmon.barry1@gmail.com>
2024-03-09 17:14:12 +0700

    Continue with React Training

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -1,9 +1,10 @@
 import Content from './Content';
 import Footer from './Footer';
 import Header from './Header';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import AddItem from './AddItem';
 import SearchItem from './SearchItem';
+import apiRequest from './apiRequest';
 
 function App() {
 const API_URL = 'http://localhost:3500/items';
@@ -34,28 +35,54 @@ const API_URL = 'http://localhost:3500/items';
       (async ()=> await fetchItems())();
     }, 2000)
 
-    (async () => await fetchItems())()
   }, [])
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+    const result =  await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
 
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
-    setAndSaveItems(listItems);
+    setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myItem[0].checked})
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
+
+    const deleteOptions = {method: "DELETE"};
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
   }
 
-  const handeSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     if (!newItem) return;
     addItem(newItem);
@@ -68,7 +95,7 @@ const API_URL = 'http://localhost:3500/items';
       <AddItem 
         newItem={newItem}
         setNewItem={setNewItem}
-        handleSubmit={handeSubmit}
+        handleSubmit={handleSubmit}
       />
       <SearchItem
         search={search}
