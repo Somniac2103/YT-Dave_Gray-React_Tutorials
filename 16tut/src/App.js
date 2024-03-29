a2f91020a2dba9b18573a4fad31d6d68db7dbdda
Barry Keith Salmon <salmon.barry1@gmail.com>
2024-03-29 17:10:46 +0800

    Decided not to complete course as it content is too outdated to follow along

--- 16tut/src/App.js
+++ 16tut/src/App.js
@@ -6,29 +6,28 @@ import NewPost from './NewPost';
 import PostPage from './PostPage';
 import About from './About';
 import Missing from './Missing';
-import { Route, Routes, useHistory } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 
 function App() {
   return (
+    
     <div className="App">
+      
       <Header />
       <Nav />
-      <Routes>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/post">
-          <NewPost />
-        </Route>
-        <Route path="/post/:id">
-          <PostPage />
-        </Route>
-        <Route path = "/about" component={About} />       
-        <Route path = "*" component={Missing} />
-      </Routes>      
-      <Footer />      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element= {<Home />}/>
+          <Route path="/post" element= {<NewPost />}/>
+          <Route path="/post/:id" element={<PostPage /> }/>
+          <Route path = "/about" element={About} />       
+          <Route path = "*" element={Missing} />
+        </Routes> 
+      </BrowserRouter>       
+      <Footer /> 
+         
     </div>
   );
 }
